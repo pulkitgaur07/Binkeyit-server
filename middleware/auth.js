e9e24e3e81790fc462cbf70b1f3d09e5b62cf8f0
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-19 17:37:53 +0530

    Changes done

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,16 @@ import jwt from 'jsonwebtoken';
 const auth = async (request,response,next)=>{
     try{
         const token = request.cookies.accessToken || request?.headers?.authorization?.split(" ")[1] ///["Bearer","token"]
-        console.log("token", token);
 
         if(!token){
             return response.status(401).json({
-                message : "Sign in or create an account to continue.",
+                message : "Provide token",
                 error : true,
                 success : false
             })
         }
         
-        const decode = jwt.verify(token,process.env.SECRET_KEY_ACCESS_TOKEN);
+        const decode = await jwt.verify(token,process.env.SECRET_KEY_ACCESS_TOKEN);
 
         if(!decode){
             return response.status(401).json({
